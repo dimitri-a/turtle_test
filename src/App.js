4fe422b5ffef80d72440a1522fab9748ff6bf972
dimitric <couwenberg@gmail.com>
2017-10-17 07:24:50 +1100

    PLACE command

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ class App extends Component {
             x: 0,
             y: 0,
             command: '',
-            facing: 'EAST'
+            facing: 'EAST',
+            init:false
         }
     }
 
     //processes commands
     process = () => {
+        let init=false;
 
         const command = document.getElementById('val').value;
 
@@ -25,86 +27,102 @@ class App extends Component {
         this.setState({command:command});
 
         // is there a valid PLACE COMMAND?
+        if (command.indexOf('PLACE') > -1)
+        {
+            const isValid =/^[A-Z]+ [0-4],[0-4] [A-Z]+$/.test(command)
+            console.log('isValid=',isValid);
 
-        const isValid =/^[A-Z]+ [0-4],[0-4] [A-Z]+$/.test(command)
-        console.log('isValid=',isValid);
+            if (!isValid) return;
+            this.setState({init:true});
+            let placeArr= command.split(' ');
+            console.log('placeArr=',placeArr);
 
-        if (!isValid) return;
+            //coordinates
+            let coorArr =placeArr[1].split(',');
 
+            this.setState({x:coorArr[0]});
+            this.setState({y:coorArr[1]});
+            this.setState({facing:placeArr[2]});
+          console.log('coorArr=',coorArr);
+            init=true;
+        }
+
+        console.log('this.state',this.state);
         //todo remove
         debugger;
-        switch (command) {
 
-            case 'MOVE':
+        //if there was a valid PLACE command
+        if (init || this.state.init) {
+            switch (command) {
+                case 'MOVE':
+                    switch (this.state.facing) {
+                        case 'EAST':
+                            this.setState({x: this.state.x + 1});
+                            break;
 
-                switch (this.state.facing) {
-                    case 'EAST':
-                        this.setState({x: this.state.x + 1});
-                        break;
+                        case 'SOUTH':
+                            this.setState({y: this.state.y - 1});
+                            break;
 
-                    case 'SOUTH':
-                        this.setState({y: this.state.y - 1});
-                        break;
+                        case 'WEST':
+                            this.setState({x: this.state.x - 1});
+                            break;
 
-                    case 'WEST':
-                        this.setState({x: this.state.x - 1});
-                        break;
+                        case 'NORTH':
+                            this.setState({y: this.state.y + 1});
+                            break;
+                    }
+                    break;
 
-                    case 'NORTH':
-                        this.setState({y: this.state.y + 1});
-                        break;
-                }
-                break;
+                case 'LEFT':
 
-            case 'LEFT':
+                    switch (this.state.facing) {
+                        case 'EAST':
+                            this.setState({facing: 'NORTH'});
+                            break;
 
-                switch (this.state.facing) {
-                    case 'EAST':
-                        this.setState({facing: 'NORTH'});
-                        break;
+                        case 'SOUTH':
+                            this.setState({facing: 'EAST'});
+                            break;
 
-                    case 'SOUTH':
-                        this.setState({facing: 'EAST'});
-                        break;
+                        case 'WEST':
+                            this.setState({facing: 'SOUTH'});
+                            break;
 
-                    case 'WEST':
-                        this.setState({facing: 'SOUTH'});
-                        break;
+                        case 'NORTH':
+                            this.setState({facing: 'WEST'});
+                            break;
+                    }
+                    break;
 
-                    case 'NORTH':
-                        this.setState({facing: 'WEST'});
-                        break;
-                }
-                break;
+                case 'RIGHT':
+                    switch (this.state.facing) {
+                        case 'EAST':
+                            this.setState({facing: 'SOUTH'});
+                            break;
 
-            case 'RIGHT':
-                switch (this.state.facing) {
-                    case 'EAST':
-                        this.setState({facing: 'SOUTH'});
-                        break;
+                        case 'SOUTH':
+                            this.setState({facing: 'WEST'});
+                            break;
 
-                    case 'SOUTH':
-                        this.setState({facing: 'WEST'});
-                        break;
+                        case 'WEST':
+                            this.setState({facing: 'NORTH'});
+                            break;
 
-                    case 'WEST':
-                        this.setState({facing: 'NORTH'});
-                        break;
+                        case 'NORTH':
+                            this.setState({facing: 'EAST'});
+                            break;
+                    }
+                    break;
 
-                    case 'NORTH':
-                        console.log('sss');
-                        this.setState({facing: 'EAST'});
-                        break;
-                }
-                break;
+                case 'REPORT':
+                    console.log('REPORT x=', this.state.x);
+                    console.log('REPORT y=', this.state.y);
+                    console.log('REPORT facing=', this.state.facing);
 
-            case 'REPORT':
-                console.log('REPORT x=', this.state.x);
-                console.log('REPORT y=', this.state.y);
-                console.log('REPORT facing=', this.state.facing);
-
-            default:
-                break;
+                default:
+                    break;
+            }
         }
 
     }
