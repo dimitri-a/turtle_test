792943828f9475d71ed63e5e167ea74cef9fe217
Couwenberg <Dimitri.Couwenberg@DET>
2017-10-17 10:19:49 +1100

    css update

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 
 export default class Box extends Component {
+
     render() {
 
         var indents = [];
         for (var i = 0; i < 25; i++) {
-            indents.push(<div className="box box1">1</div>);
+
+            indents.push(<div id={i} >{i}</div>);
+
         }
         console.log('indents=',indents);
         return (
