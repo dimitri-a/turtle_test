fa1694e6314e709af28aa5ccf542b8c042be1433
dimitric <couwenberg@gmail.com>
2017-10-16 21:08:30 +1100

    created appcontainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,107 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Grid from './components/Grid';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <Grid/>
-      </div>
-    );
-  }
+
+
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {
+            x: 0,
+            y: 0,
+            command: 'PLACE 3,,EAST MOVE',
+            facing: 'NORTH'
+        }
+    }
+
+    //processes commands
+    process = () => {
+        
+        console.log('process');
+
+        const val = document.getElementById('val').value;
+
+        console.log('value', val);
+
+        //this.setState({currentCommand: val});
+
+        console.log('this.state.currentCommand=', this.state.command);
+
+        const cmds = this.state.command.split(' ');
+        console.log('cmds=', cmds);
+
+        cmds.every(c => {
+            switch (c) {
+                case 'PLACE':
+
+                    break;
+
+                case 'MOVE':
+                    this.state.facing === 'LEFT' ? this.setState({x: this.state.x - 1}) : this.setState({x: this.state.x + 1})
+                    break;
+
+                case 'LEFT':
+
+                    switch (this.state.facing) {
+                        case 'EAST':
+                            this.setState({facing: 'NORTH'});
+                            break;
+
+                        case 'SOUTH':
+                            this.setState({facing: 'EAST'});
+                            break;
+
+                        case 'WEST':
+                            this.setState({facing: 'SOUTH'});
+                            break;
+
+                        case 'NORTH':
+                            this.setState({facing: 'WEST'});
+                            break;
+                    }
+
+                case 'RIGHT':
+
+                    switch (this.state.facing) {
+                        case 'EAST':
+                            this.setState({facing: 'SOUTH'});
+                            break;
+
+                        case 'SOUTH':
+                            this.setState({facing: 'WEST'});
+                            break;
+
+                        case 'WEST':
+                            this.setState({facing: 'NORTH'});
+                            break;
+
+                        case 'NORTH':
+                            this.setState({facing: 'EAST'});
+                            break;
+                    }
+                case 'REPORT':
+                    console.log('x=', this.state.x);
+                    console.log('y=', this.state.y);
+                    console.log('facing=', this.state.facing);
+                default:
+                    break;
+            }
+        })
+
+
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <Grid x={this.state.x} y={this.state.y} facing={this.state.facing} process={this.process}/>
+            </div>
+        )
+            ;
+    }
 }
 
 export default App;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,72 +5,20 @@ class Grid extends Component {
     constructor(props) {
         super();
         this.props = props;
-        this.state = {
-            x: 0,
-            y: 0,
-            command: '',
-            facing: ''
-        }
     }
 
-    //processes commands
-    process = () => {
-
-        const val = document.getElementById('val').value;
-
-        console.log('value', val);
-
-        this.setState({currentCommand: val});
-
-        console.log('this.state.currentCommand=', this.state.currentCommand);
-
-        this.state.currentCommand.each(c => {
-            switch (c) {
-                case 'PLACE':
-
-                    break;
-
-                case 'MOVE':
-                    this.state.facing === 'LEFT' ? this.setState({x: this.state.x - 1}) : this.setState({x: this.state.x + 1})
-                    break;
-
-                case 'LEFT':
-
-                    switch (this.state.facing) {
-                        case 'EAST':
-                            this.setState({facing: 'NORTH'});
-                            break;
-
-                        case 'SOUTH':
-                            this.setState({facing: 'EAST'});
-                            break;
-
-                        case 'WEST':
-                            this.setState({facing: 'SOUTH'});
-                            break;
-
-                        case 'NORTH':
-                            this.setState({facing: 'WEST'});
-                            break;
-                    }
-
-                default:
-                    break;
-            }
-        })
-
-
-    }
 
     render() {
         return (
             <div className="Grid">
 
                 <input id='val' type='text'/>
-                <button onClick={this.process}>Go</button>
+                <button onClick={this.props.process}>Go</button>
 
                 <pre>
-              {JSON.stringify(this.state.currentPosition, null, 2)}
+              {JSON.stringify(this.props.x, null, 2)},
+                    {JSON.stringify(this.props.y, null, 2)}
+                    {JSON.stringify(this.props.facing, null, 2)}
             </pre>
 
 
