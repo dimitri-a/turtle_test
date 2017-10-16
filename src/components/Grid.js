5414d8736b6b1200b9715496bcea6cc00454e6d9
dimitric <couwenberg@gmail.com>
2017-10-16 20:13:40 +1100

    start process

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,14 +1,54 @@
-import React, { Component } from 'react';
-import './Grid.css';
+import React, {Component} from 'react';
 
 class Grid extends Component {
-  render() {
-    return (
-      <div className="Grid">
-        grid
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super();
+        this.props = props;
+        this.state = {
+            x: 0,
+            y: 0,
+            command: '',
+            direction: ''
+        }
+    }
+
+    //processes commands
+    process = () => {
+
+        const val = document.getElementById('val').value;
+
+        console.log('value', val);
+
+        this.setState({currentCommand: val});
+
+        console.log('this.state.currentCommand=', this.state.currentCommand);
+
+        switch (this.state.currentCommand) {
+            case 'MOVE':
+                this.state.direction === 'LEFT' ? this.setState({x: this.state.x - 1}) : this.setState({x: this.state.x + 1})
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="Grid">
+
+                <input id='val' type='text'/>
+                <button onClick={this.process}>Go</button>
+
+                <pre>
+              {JSON.stringify(this.state.currentPosition, null, 2)}
+            </pre>
+
+
+            </div>
+        );
+    }
 }
 
 export default Grid;
