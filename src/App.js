d98c349af98a75e37a2f72cf0a778a723f823484
dimitric <couwenberg@gmail.com>
2017-10-17 22:02:54 +1100

    fixed conversion issue

--- src/App.js
+++ src/App.js
@@ -61,14 +61,13 @@ class App extends Component {
     //processes this.state.commands
     process = (command) => {
         let init = false;
-        
-        console.log('app.js this should be the command from the Grid comp=',command);
+
+        console.log('app.js this should be the command from the Grid comp=', command);
 
         console.log('APP.js starting App.process()');
-        console.log('APP.js ,check to see setstate this.state.x=', this.state.x);
-        console.log('this.state.y=', this.state.y);
+        console.log('APP.js ,check to see setstate this.state', this.state);
 
-        this.setState({command: command},() => {
+        this.setState({command: command}, () => {
             // check to see there a valid PLACE this.state.command?
             if (this.state.command.indexOf('PLACE') > -1) {
                 const isValid = /^[A-Z]+ [0-4],[0-4] [A-Z]+$/.test(this.state.command)
@@ -83,97 +82,95 @@ class App extends Component {
 
                 //set position
                 let coorArr = placeArr[1].split(',');
-                this.setState({x: coorArr[0]});
-                this.setState({y: coorArr[1]});
+                this.setState({x: Number(coorArr[0])});
+                this.setState({y: Number(coorArr[1])});
                 this.setState({facing: placeArr[2]});
 
-                console.log('app.js process, new x value=',coorArr[0]);
-                console.log('app.js process, new y value=',coorArr[1]);
+                console.log('app.js process, new x value=', coorArr[0]);
+                console.log('app.js process, new y value=', coorArr[1]);
 
             }
+
+            //if there was a valid PLACE this.state.command
+            if (init || this.state.init) {
+                console.log('app.js start init this.state.command = ', this.state.command);
+                switch (this.state.command) {
+                    case 'MOVE':
+                        switch (this.state.facing) {
+                            case 'EAST':
+                                this.setState({x: this.state.x + 1});
+                                break;
+
+                            case 'SOUTH':
+                                this.setState({y: this.state.y - 1});
+                                break;
+
+                            case 'WEST':
+                                this.setState({x: this.state.x - 1});
+                                break;
+
+                            case 'NORTH':
+                                console.log('NORTH facing this.state=', this.state);
+                                this.setState({y: this.state.y + 1}, () =>{console.log('hopefully y is changed!!',this.state)});
+                                break;
+                        }
+                        break;
+
+                    case 'LEFT':
+
+                        switch (this.state.facing) {
+                            case 'EAST':
+                                this.setState({facing: 'NORTH'});
+                                break;
+
+                            case 'SOUTH':
+                                this.setState({facing: 'EAST'});
+                                break;
+
+                            case 'WEST':
+                                this.setState({facing: 'SOUTH'});
+                                break;
+
+                            case 'NORTH':
+                                this.setState({facing: 'WEST'});
+                                break;
+                        }
+                        break;
+
+                    case 'RIGHT':
+                        switch (this.state.facing) {
+                            case 'EAST':
+                                this.setState({facing: 'SOUTH'});
+                                break;
+
+                            case 'SOUTH':
+                                this.setState({facing: 'WEST'});
+                                break;
+
+                            case 'WEST':
+                                this.setState({facing: 'NORTH'});
+                                break;
+
+                            case 'NORTH':
+                                this.setState({facing: 'EAST'});
+                                break;
+                        }
+                        break;
+
+                    // case 'REPORT':
+                    //     console.log('REPORT x=', this.state.x);
+                    //     console.log('REPORT y=', this.state.y);
+                    //     console.log('REPORT facing=', this.state.facing);
+
+                    default:
+                        break;
+                }
+            }
+
+            console.log('APP.JS test these values again:this.state=', this.state);
+
 
         });
-
-
-        //if there was a valid PLACE this.state.command
-        if (init || this.state.init) {
-            //todo remove
-            debugger;
-            switch (this.state.command) {
-                case 'MOVE':
-                    switch (this.state.facing) {
-                        case 'EAST':
-                            this.setState({x: this.state.x + 1});
-                            break;
-
-                        case 'SOUTH':
-                            this.setState({y: this.state.y - 1});
-                            break;
-
-                        case 'WEST':
-                            this.setState({x: this.state.x - 1});
-                            break;
-
-                        case 'NORTH':
-                            console.log('NORTH facing this.state.x=', this.state.x);
-                            console.log('this.state.y=', this.state.y);
-                            this.setState({y: this.state.y + 1});
-                            break;
-                    }
-                    break;
-
-                case 'LEFT':
-
-                    switch (this.state.facing) {
-                        case 'EAST':
-                            this.setState({facing: 'NORTH'});
-                            break;
-
-                        case 'SOUTH':
-                            this.setState({facing: 'EAST'});
-                            break;
-
-                        case 'WEST':
-                            this.setState({facing: 'SOUTH'});
-                            break;
-
-                        case 'NORTH':
-                            this.setState({facing: 'WEST'});
-                            break;
-                    }
-                    break;
-
-                case 'RIGHT':
-                    switch (this.state.facing) {
-                        case 'EAST':
-                            this.setState({facing: 'SOUTH'});
-                            break;
-
-                        case 'SOUTH':
-                            this.setState({facing: 'WEST'});
-                            break;
-
-                        case 'WEST':
-                            this.setState({facing: 'NORTH'});
-                            break;
-
-                        case 'NORTH':
-                            this.setState({facing: 'EAST'});
-                            break;
-                    }
-                    break;
-
-                // case 'REPORT':
-                //     console.log('REPORT x=', this.state.x);
-                //     console.log('REPORT y=', this.state.y);
-                //     console.log('REPORT facing=', this.state.facing);
-
-                default:
-                    break;
-            }
-        }
-
-        console.log('APP.JS test these values again:this.state=', this.state);
 
 
     }
