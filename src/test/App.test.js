06822f48c823c7e5a9fd499c13ec08d325432c20
dimitric <couwenberg@gmail.com>
2017-10-18 09:43:44 +1100

    updated unittest

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createRenderer } from 'react-test-renderer/shallow';
+import {createRenderer} from 'react-test-renderer/shallow';
 import App from '../App';
 
 it('does it render', () => {
 
     const props = {
-      addTodo: jest.fn()
+        addTodo: jest.fn()
     }
 
     const renderer = createRenderer();
-    renderer.render(< App />)
+    renderer.render(< App/>)
     const output = renderer.getRenderOutput()
 
     expect(output.type).toBe('div');
-   // expect(output.props.className).toBe('header')
-    //
-    // output.props.children.props.onSave('fsakhfkjsdh');
-    // expect(props.addTodo).toBeCalled()
+    expect(output.props.className).toBe('App container')
 
-  });
+});
